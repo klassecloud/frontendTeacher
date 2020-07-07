e18521057a3bee53b69b94757883226bdc9e6a66
Franiboy <63066013+Franiboy@users.noreply.github.com>
2020-07-07 12:12:14 +0200

    Toggle only Enddate hinzugefügt

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -12,19 +12,20 @@ import { Tasks } from '../task-data';
 export class CalendarComponent implements OnInit{
 
   @Input() tasks: Task_Interface[] = [];
+  enddate = false;
 
+  // @ts-ignore
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     weekends: true, // initial value
-    events: []
+    events: [],
   };
 
   toggleWeekends() {
-    this.calendarOptions.weekends = !this.calendarOptions.weekends // toggle the boolean!
+    this.calendarOptions.weekends = !this.calendarOptions.weekends; // toggle the boolean!
   }
-  constructor() {
-  }
-  ngOnInit() {
+  toggleEndDate()
+  {
     this.tasks = Tasks;
     const name = [];
     const start = [];
@@ -32,8 +33,14 @@ export class CalendarComponent implements OnInit{
     const color = [];
     for (let index = 0; index < this.tasks.length; index++)
     {
-      name.push(Tasks[index].name);
-      start.push(Tasks[index].start);
+      if (this.enddate) {
+        name.push('Abgabe: ' + Tasks[index].name);
+        start.push(Tasks[index].end);
+      }
+      else {
+        name.push(Tasks[index].name);
+        start.push(Tasks[index].start);
+      }
       end.push(Tasks[index].end);
       if (Tasks[index].uebung) {
         color.push('green');
@@ -41,12 +48,18 @@ export class CalendarComponent implements OnInit{
       else { color.push('purple'); }
     }
     const newEvents = [];
+    // tslint:disable-next-line:prefer-for-of
     for (let index = 0; index < this.tasks.length; index++)
     {
-      newEvents.push({ title: name.pop(), color: color.pop(), start: start.pop(), end: end.pop() });
+        newEvents.push({ title: name.pop(), color: color.pop(), start: start.pop(), end: end.pop() });
     }
-
     this.calendarOptions.events = newEvents;
+    this.enddate = !this.enddate;
+  }
+  constructor() {
+  }
+  ngOnInit() {
+    this.toggleEndDate();
   }
 
 }
